13905a01dc5b4aeb77e6b2e0f430c4fa973150aa
Anshu Priya <raj162005saurav@gmail.com>
2025-02-05 22:59:59 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -7,9 +7,9 @@ class Solution:
             result.append(num)  # Add each element again
         return result
      
-  Explaination: 
-The method getConcatenation takes a list of integers (nums).
-It creates an empty list result.
-It then iterates over nums twice and appends each element to result during each iteration. This effectively "duplicates" the original list, 
-meaning the final result contains two copies of nums concatenated together.
-Finally, it returns the concatenated 
+#   Explaination: 
+# The method getConcatenation takes a list of integers (nums).
+# It creates an empty list result.
+# It then iterates over nums twice and appends each element to result during each iteration. This effectively "duplicates" the original list, 
+# meaning the final result contains two copies of nums concatenated together.
+# Finally, it returns the concatenated 
