5da97c4c38c01e99a458d5185de5672999dc0113
Anshu Priya <raj162005saurav@gmail.com>
2025-02-21 22:19:09 +0530

    Update Task25.py

--- Task25.py
+++ Task25.py
@@ -1,21 +1,11 @@
+# 21-02-2025
 class Solution:
     def intersection(self, nums1, nums2):
-        # Convert both arrays to sets
         set1 = set(nums1)
         set2 = set(nums2)
-        
-        # Return the intersection of both sets
         return list(set1 & set2)
-
-# Create an instance of Solution class
 solution = Solution()
-
-# Test cases
 nums1 = [4, 9, 5]
 nums2 = [9, 4, 9, 8, 4]
-
-# Get the intersection
 result = solution.intersection(nums1, nums2)
-
-# Print the result
-print(result)  # Output: [9, 4]
+print(result)  
